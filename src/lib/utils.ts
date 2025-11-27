0b7a8a5abc423819c362b046c8843fa8b5f4f7a6
Wahid <decode.std@gmail.com>
2025-11-27 22:34:44 +0700

    add http client and proxy api

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,23 @@
-// src/lib/utils.ts
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const reqQueryUrl = (
+  endpoint: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  query: any
+): string => {
+  const params = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(query)) {
+    if (value === null || value === undefined || value === "") continue;
+    params.append(key, String(value));
+  }
+
+  const queryString = params.toString();
+  return queryString ? `${endpoint}?${queryString}` : endpoint;
+};
