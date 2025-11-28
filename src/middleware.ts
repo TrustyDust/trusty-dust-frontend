65c3ad2ea873b3782972395d325d23802f85cd76
Wahid <decode.std@gmail.com>
2025-11-29 03:40:50 +0700

    add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,23 +17,24 @@ export async function middleware(request: NextRequest) {
     const publicPages = [signIn as string];
     const isPublicPage = publicPages.includes(pathname);
 
-    const redirect = (url: string) => {
-        if (pathname === url) {
-            return NextResponse.next();
-        }
-        return NextResponse.redirect(new URL(url, request.url));
-    };
+    const redirect = (url: string) =>
+        NextResponse.redirect(new URL(url, request.url));
 
     if (pathname === "/") {
         if (jwt) {
-            return NextResponse.next();
+            return redirect(home);
         } else {
             return redirect(signIn);
         }
     }
 
-    if (jwt && isPublicPage) return redirect(home);
-    if (!jwt && !isPublicPage) return redirect(signIn);
+    if (jwt && isPublicPage) {
+        return redirect(home);
+    }
+
+    if (!jwt && !isPublicPage) {
+        return redirect(signIn);
+    }
 
     return NextResponse.next();
 }
