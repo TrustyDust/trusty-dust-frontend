fcc54dc5d414f6a186dbf9f9494b5656fbd3f889
Wahid <decode.std@gmail.com>
2025-11-29 11:20:19 +0700

    fix sign in method and add loading animation pop up

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -361,6 +361,20 @@ export function AuthProvider({
     }
   }, [disconnect, privyLogout, router])
 
+  const connectWithPrivy = () => {
+    connectWithPrivyOrigin()
+  }
+
+  const connectWithRainbow = () => {
+    if (isConnected) {
+      disconnect()
+      openConnectModal && openConnectModal()
+    } else {
+      openConnectModal && openConnectModal()
+    }
+  }
+
+
   const value = useMemo<AuthContextType>(
     () => ({
       isAuthenticated,
